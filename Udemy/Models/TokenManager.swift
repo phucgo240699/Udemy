96b4444219c78f2b82c7cb7348bbc90326356475
phucgo240699 <phucgo240699@gmail.com>
2020-09-29 10:21:41 +0700

    MVC and Resources

---
+++ Udemy/Models/TokenManager.swift
@@ -0,0 +1,13 @@
+//
+//  TokenManager.swift
+//  Udemy
+//
+//  Created by Phúc Lý on 9/29/20.
+//  Copyright © 2020 Phúc Lý. All rights reserved.
+//
+
+import Foundation
+
+class TokenManager {
+    static let shared = TokenManager()
+}
