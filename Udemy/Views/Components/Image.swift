720121deefdae4782379acfdfe5e1e1b46c1d49f
phucgo240699 <phucgo240699@gmail.com>
2020-09-29 11:05:03 +0700

    add app icon, setup frame for components

---
+++ Udemy/Views/Components/Image.swift
@@ -0,0 +1,9 @@
+//
+//  Image.swift
+//  Udemy
+//
+//  Created by Phúc Lý on 9/29/20.
+//  Copyright © 2020 Phúc Lý. All rights reserved.
+//
+
+import Foundation
---
+++ Udemy/Views/Components/TextView.swift
@@ -0,0 +1,9 @@
+//
+//  TextView.swift
+//  Udemy
+//
+//  Created by Phúc Lý on 9/29/20.
+//  Copyright © 2020 Phúc Lý. All rights reserved.
+//
+
+import Foundation
