640cc9d2abba1bfdbeeb1e88f5386b63ffeb9eca
phucgo240699 <phucgo240699@gmail.com>
2020-10-12 03:27:42 +0700

    don't save unessential properties of account

--- Udemy/Models/Account.swift
+++ Udemy/Models/Account.swift
@@ -21,13 +21,6 @@ class Account {
     init() {
         self.email = UserDefaults.standard.string(forKey: "email")
         self.password = UserDefaults.standard.string(forKey: "password")
-        self.name = UserDefaults.standard.string(forKey: "name")
-        self.phone = UserDefaults.standard.string(forKey: "phone")
-        self.gender = UserDefaults.standard.string(forKey: "gender")
-        self.role = UserDefaults.standard.string(forKey: "role")
-        self.image = UserDefaults.standard.string(forKey: "image")
-        self.address = UserDefaults.standard.string(forKey: "address")
-        self.description = UserDefaults.standard.string(forKey: "description")
     }
     
     func update(_ email: String?, _ password: String?, _ name: String?, _ phone: String?, _ gender: String?, _ role: String?, _ image: String?, _ address: String?, _ description: String?) {
@@ -54,13 +47,6 @@ class Account {
         self.description = description
         UserDefaults.standard.set(self.email, forKey: "email")
         UserDefaults.standard.set(self.password, forKey: "password")
-        UserDefaults.standard.set(self.name, forKey: "name")
-        UserDefaults.standard.set(self.phone, forKey: "phone")
-        UserDefaults.standard.set(self.gender, forKey: "gender")
-        UserDefaults.standard.set(self.role, forKey: "role")
-        UserDefaults.standard.set(self.image, forKey: "image")
-        UserDefaults.standard.set(self.address, forKey: "address")
-        UserDefaults.standard.set(self.description, forKey: "description")
     }
     
     func release() {
